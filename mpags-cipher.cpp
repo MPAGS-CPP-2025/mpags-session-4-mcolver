2dc250d4fa00866f00ad8a164ff611edc82d0e5f
mcolver <maddie.colver@bristol.ac.uk>
2025-11-09 15:26:38 +0000

    Add help text

--- mpags-cipher.cpp
+++ mpags-cipher.cpp
@@ -1,9 +1,27 @@
 #include <cctype>
 #include <iostream>
 #include <string>
+#include <vector>
 
-int main()
+int main(int argc, char* argv[])
 {
+    const std::vector<std::string> cmdLineArgs { argv, argv+argc };
+
+    for(int i{0}; i < argc; i++) {
+        if(cmdLineArgs[i] == "--help" || cmdLineArgs[i] == "-h") {
+            std::cout << "This programs encrypts and decrypts input text to and from a cipher your your choice.\n"
+                         " \n"
+                         "Usage:\n"
+                         "-c type: Provide cipher type. Currently supported: None\n"
+                         "-i filename: Provide input file. If not provided, input from terminal.\n"
+                         "-o filename: Provide output file. If not provided, output to terminal.\n"
+                         "-k key: Provide cipher key. Currently supported: None\n"
+                         "--encrypt: Encrypt input text. Default action.\n"
+                         "--decrypt: Decrypt input text.\n" << std::endl;
+            return 0;
+        }
+    }
+
     // Initialise variables
     char c{'x'};
     std::string out_text;
